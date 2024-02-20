dda1cf98ebfe52a51e6ceac9f18aacde9e0c1b0d
Erik Aghajanyan <eroagajanyan@mail.ru>
2024-02-20 21:18:03 +0400

    asdasds

--- lesson1/task1/Program.cs
+++ lesson1/task1/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello world");
+﻿// Console.WriteLine("Hello world");
+
+
+int bulki = 100;
+Console.WriteLine(bulki);
+
+double a = 5.55;
+char b = '@';
+bool c = true;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
