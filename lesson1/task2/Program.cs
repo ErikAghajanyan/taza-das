ff14ae3947024448e8adf91367210b3d5f144fef
Erik Aghajanyan <eroagajanyan@mail.ru>
2024-02-20 22:09:22 +0400

    as

--- lesson1/task2/Program.cs
+++ lesson1/task2/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int i1 = 5;
+ int i2 = 6;
+int sum = i1 + i2;
+
+Console.WriteLine(sum);
+long l1 = 7;
+long sum2 = l1 + i1;
+Console.WriteLine(sum2);
+
+double w1 = 4.2;
+double sum3 = w1 + i1;
+Console.WriteLine(sum3);
+
+int p = 4;
+int l = 10;
+int m = 15;
+int f = 25;
+int summm = p * l / (m + f);
+Console.WriteLine(summm);
